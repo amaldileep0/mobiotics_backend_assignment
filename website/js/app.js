a90f7b864d0c77705e0aba5619fa0f1f4bf24d43
Amal Dileep <amaldileep0@gmail.com>
2019-10-23 16:06:19 +0530

    updates for web page

--- website/js/app.js
+++ website/js/app.js
@@ -1,5 +1,54 @@
 $(function () {
     "use strict";
+    //div
+	var loginDiv = $('#_login');
+	var registerDiv = $('#_register');
+	var forgetPasswordDiv = $('#_forget_password');
+	var resetPasswordDiv = $('#_reset_password');
+	var requestListingDiv = $('#_request_listing');
+	var createRequestDiv  = $('#_create_request');
+
+	$('.register_link').click(function(event) {
+    	event.preventDefault();
+    	registerDiv.show();
+        resetForm('register-from')
+    	loginDiv.hide();
+    	forgetPasswordDiv.hide();
+    	resetPasswordDiv.hide();
+    	requestListingDiv.hide();
+    	createRequestDiv.hide();
+	});
+
+	$('.login_link').click(function(event) {
+    	event.preventDefault();
+    	loginDiv.show();
+        resetForm('login-form')
+    	registerDiv.hide();
+    	forgetPasswordDiv.hide();
+    	resetPasswordDiv.hide();
+    	requestListingDiv.hide();
+    	createRequestDiv.hide();
+	});
+
+	$('.forget_password_link').click(function(event) {
+    	event.preventDefault();
+    	forgetPasswordDiv.show();
+        resetForm('forget-password-form')
+    	loginDiv.hide();
+    	registerDiv.hide();
+    	resetPasswordDiv.hide();
+    	requestListingDiv.hide();
+    	createRequestDiv.hide();
+	});
+
+	function resetForm(formId) {
+        var arr = ['login-form', 'register-form', 'forget-password-form'];
+        for (var key in arr) {
+            if (arr[key] != formId) {
+                document.getElementById(arr[key]).reset();
+            }
+        }
+	}
 
     $( document ).ready(function() {
     	$( "#closed" ).datepicker();
